876afd9713d83452ae225a070006f3291a839bb1
Daniel Riccio <ricciodaniel98@gmail.com>
2024-01-15 18:56:41 -0500

    add error handling

--- src/api/getNewPasskey.ts
+++ src/api/getNewPasskey.ts
@@ -2,10 +2,18 @@ import { PUBLIC_API_URL } from '$env/static/public';
 import axios from 'axios';
 
 export async function getNewPasskey(): Promise<string | null> {
-	const passkey = (await axios.get<{ passkey: string }>(`${PUBLIC_API_URL}/passkey`)).data.passkey;
+	try {
+		const response = await axios.get<{ passkey: string }>(`${PUBLIC_API_URL}/passkey`);
+		const passkey = response.data.passkey;
 
-	if (!passkey) {
+		if (!passkey) {
+			return null;
+		}
+
+		return passkey;
+	} catch (error) {
+		// Handle error here
+		console.error('Error occurred while fetching passkey:', error);
 		return null;
 	}
-	return passkey;
 }
