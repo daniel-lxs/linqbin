dabe677e1541df29561ca6748538d7f69ecdc58f
Daniel Riccio <ricciodaniel98@gmail.com>
2024-01-17 15:28:53 -0500

    add source to about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,9 +3,10 @@
 </script>
 
 <svelte:head>
-	<title>Linqbin</title>
+	<title>About | Linqbin</title>
 	<meta name="description" content="Create temporary links with Linqbin" />
 </svelte:head>
+
 <div class="flex items-center justify-center h-full">
 	<Card>
 		<header class="card-header">
@@ -16,13 +17,14 @@
 			<div class="mt-4">
 				<h3 class="text-lg font-semibold mb-2">What is Linqbin?</h3>
 				<p>
-					Linqbin is a project designed to serve as a temporary and encrypted link shortner service.
+					<a href="https://github.com/daniel-lxs/linqbin" class="text-blue-500">Linqbin</a> is a project
+					designed to serve as a temporary and encrypted link shortener service.
 				</p>
 
 				<h3 class="text-lg font-semibold mt-4 mb-2">What does it do?</h3>
 				<p>
-					Linqbin you can create links that contain either plain text or URL(s). If only one URL is
-					provided on your entry, Linqbin will automatically redirect to that URL. Every link
+					Linqbin allows you to create links that contain either plain text or URL(s). If only one
+					URL is provided on your entry, Linqbin will automatically redirect to that URL. Every link
 					generated is encrypted and temporary, allowing you to set a specific duration or number of
 					visits after which the link will expire.
 				</p>
@@ -43,15 +45,15 @@
 						functionality
 					</li>
 				</ul>
+				<p class="mt-4">
+					Linqbin is completely open source and free to use. The project is licensed under the <a
+						href="https://github.com/daniel-lxs/linqbin/blob/main/LICENSE.md"
+						class="text-blue-500">MIT</a
+					>
+					license, so it is provided freely and as-is, with no warranty. You can find the source code
+					on <a href="https://github.com/daniel-lxs/linqbin" class="text-blue-500">GitHub</a>.
+				</p>
 			</div>
-
-			<p class="mt-4">
-				Linqbin is completely open source and free to use. The project is licensed under the <a
-					href="https://github.com/daniel-lxs/linqbin/blob/main/LICENSE.md"
-					class="text-blue-500">MIT</a
-				>
-				license, so it is provided freely and as-is, with no warranty.
-			</p>
 		</section>
 	</Card>
 </div>
