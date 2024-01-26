7d7cf3870a7ffc58d63b147b709f836b2fbcb965
Daniel Riccio <ricciodaniel98@gmail.com>
2024-01-26 15:19:02 -0500

    separate create new link button component

---
+++ src/lib/components/simple/CreateLinkButton.svelte
@@ -0,0 +1,8 @@
+<div class="flex items-center justify-center">
+	<button
+		class="btn variant-filled-primary h-10 mb-2 mt-4"
+		on:click={() => (window.location.href = '/')}
+	>
+		Create new link
+	</button>
+</div>
