2e2ffa0ab3dfb0d57cd2dab80daf3acfdfa6060b
Daniel Riccio <ricciodaniel98@gmail.com>
2024-02-09 09:04:20 -0500

    refactor: :recycle: create generic input field

---
+++ src/lib/components/simple/InputField.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let label: string;
+	export let placeholder: string = '';
+	export let error: string = '';
+	export let value: string = '';
+</script>
+
+<div class="mt-6 w-full grid-cols-[1fr_auto] relative">
+	<div class="relative w-full md:w-1/2">
+		<label class="label mb-6">
+			<span>{label}</span>
+			<input
+				class="input h-12 pl-3 pr-9 {error ? 'input-error' : ''}"
+				type="text"
+				name={label.toLowerCase()}
+				aria-label={label}
+				{placeholder}
+				bind:value
+				on:input={() => (error = '')}
+				title={label}
+				maxlength="100"
+			/>
+			{#if error}
+				<div class="ml-2 text-error-500">{error}</div>
+			{/if}
+		</label>
+	</div>
+</div>
